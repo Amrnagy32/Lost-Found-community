e5be96d724e461ef45d6240ba17d0f90e9101f5c
Amrnagy32 <amr.nagy6432@gmail.com>
2025-12-20 04:28:12 +0300

    chore: fix folder structure and move test files

--- Desktop/Lost-Found-community_1/lostfound-frontend/src/components/PostCard.js
+++
@@ -1,9 +0,0 @@
-export function PostCard(post) {
-  return `
-    <div class="card">
-      <h3>${post.title}</h3>
-      <p class="small-muted">${post.description}</p>
-      ${post.image ? `<img class="post-img" src="${post.image}" />` : ''}
-    </div>
-  `;
-}
